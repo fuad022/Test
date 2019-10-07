a3f2be137829181251d067aaa941afd152695745
Fuad <fuad-022@mail.ru>
2019-10-07 17:02:16 +0400

    Revert "Update."
    This reverts commit 6ea0b73d

--- src/Leetcode/ReverseInteger.java
+++ src/Leetcode/ReverseInteger.java
@@ -10,6 +10,7 @@ public class ReverseInteger {
             x = x / 10;
         }
         if (temp > Integer.MAX_VALUE || temp < Integer.MIN_VALUE) return 0;
+
         return (int) temp;
     }
 
